dce2225c276e2e961295dfa8cea3a5ffb6030ffc
wonedd <vinmarv@hotmail.com>
2022-08-04 19:34:02 -0300

    initial commit

--- shared/styles.tsx
+++ shared/styles.tsx
@@ -1,81 +1,81 @@
-import { css, Global, keyframes, Keyframes } from '@emotion/react'
-import styled from '@emotion/styled'
+import { css, Global } from "@emotion/react";
+
+export const media = {
+  maxMobile: "@media(max-width:800px)",
+  minlaptop: "@media(min-width: 800px)",
+};
 
 export const globalStyles = (
   <Global
     styles={css`
-      html,
-      body {
-        padding: 3rem 1rem;
+      * {
         margin: 0;
-        background: papayawhip;
-        min-height: 100%;
-        font-family: Helvetica, Arial, sans-serif;
-        font-size: 24px;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+      }
+
+      @media (max-width: 1080px) {
+        html {
+          font-size: 93.75%;
+        }
+      }
+      @media (max-width: 720px) {
+        html {
+          font-size: 87.5%;
+        }
+      }
+
+      body {
+        -webkit-font-smoothing: antialiased;
+        text-rendering: optimizeLegibility;
+        overflow-x: hidden;
+      }
+
+      body,
+      input,
+      button {
+        font-size: 14px;
+        font-family: "Inter", sans-serif;
+      }
+      button {
+        cursor: pointer;
+      }
+      body::-webkit-scrollbar {
+        width: 4px;
+      }
+
+      body::-webkit-scrollbar-track {
+        background: var(--gray900);
+      }
+
+      body::-webkit-scrollbar-thumb {
+        background-color: var(--gray900);
+        border-radius: 20px;
+      }
+      :root {
+        --primaryWhite: #ffffff;
+
+        --pink100: #ffcae0;
+
+        --red500: #f3151b;
+
+        --red600: #e20f15;
+
+        --gray200: #e9ebee;
+
+        --gray600: #999a9b;
+
+        --gray700: #737375;
+
+        --gray900: #1c1c1c;
+
+        --green100: #a3f9b9;
+
+        --green400: #40b25c;
+
+        --green600: #1e9631;
       }
     `}
   />
-)
-
-export const basicStyles = css({
-  backgroundColor: 'white',
-  color: 'cornflowerblue',
-  border: '1px solid lightgreen',
-  borderRight: 'none',
-  borderBottom: 'none',
-  boxShadow: '5px 5px 0 0 lightgreen, 10px 10px 0 0 lightyellow',
-  transition: 'all 0.1s linear',
-  margin: '3rem 0',
-  padding: '1rem 0.5rem',
-})
-
-export const hoverStyles = css`
-  &:hover {
-    color: white;
-    background-color: lightgray;
-    border-color: aqua;
-    box-shadow: -15px -15px 0 0 aqua, -30px -30px 0 0 cornflowerblue;
-  }
-`
-export const bounce = keyframes`
-  from {
-    transform: scale(1.01);
-  }
-  to {
-    transform: scale(0.99);
-  }
-`
-
-export const Basic = styled.div`
-  ${basicStyles};
-`
-
-export const Combined = styled.div`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-`
-
-export const Pink = styled(Basic)({
-  color: 'hotpink',
-})
-
-export const BasicExtended = styled(Basic)``
-
-export const ComponentSelectorsExtended = styled.div`
-  ${BasicExtended} {
-    color: green;
-  }
-  box-shadow: -5px -5px 0 0 green;
-`
-
-export const Animated = styled.div<{ animation: Keyframes }>`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-  animation: ${({ animation }) => animation} 0.2s infinite ease-in-out alternate;
-`
+);
---
+++ src/utils/Products.ts
@@ -0,0 +1,22 @@
+import { v4 as uuid } from "uuid";
+
+export const products = [
+  {
+    id: uuid(),
+    title:
+      "Monitor LED 27'' Gamer Curvo Samsung  1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50",
+    price: 2599,
+    oldPrice: 2813.99,
+    image: "/assets/tv.png",
+    favorite: false,
+  },
+  {
+    id: uuid(),
+    title:
+      "ACER Notebook Aspire 5 A515-56-32PG, Intel Core I3 11ª geração, 4GB, 256GB SDD, Windows11, Prata",
+    price: 2924.73,
+    oldPrice: 3249.7,
+    image: "/assets/note.png",
+    favorite: false,
+  },
+];
