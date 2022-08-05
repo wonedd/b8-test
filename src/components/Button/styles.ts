269739a2a98486e91ee8cc0bf92406af26e6a723
wonedd <vinmarv@hotmail.com>
2022-08-04 23:41:06 -0300

    chore: buttons

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+
+interface ContainerProps {
+  isFavorite: boolean;
+}
+
+export const Container = styled.button<ContainerProps>`
+  all: unset;
+
+  cursor: pointer;
+
+  min-width: 240px;
+  min-height: 48px;
+
+  padding: 12px 24px;
+
+  display: flex;
+  gap: 16px;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${(props) =>
+    props.isFavorite ? "var(--green100)" : "var(--green400)"};
+
+  border-radius: 5px;
+
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  letter-spacing: -0.005em;
+
+  color: ${(props) =>
+    props.isFavorite ? "var(--gray900)" : "var(--primaryWhite)"};
+
+  transition: 0.2s;
+
+  svg {
+    font-size: 1.5rem;
+    color: var(--gray900);
+  }
+
+  &:hover {
+    background-color: var(--green600);
+  }
+`;
---
+++ src/components/Card/FavoriteButton/index.tsx
@@ -0,0 +1,39 @@
+import { AxiosRequestConfig } from "axios";
+import { useState } from "react";
+import { IoHeart, IoHeartOutline } from "react-icons/io5";
+import { api } from "../../../services/api";
+import { Container } from "./styles";
+
+export function FavoriteButton(id: { id: string }) {
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+
+  const handleFavorite = async () => {
+    try {
+      await api.post("products", id);
+
+      setIsFavorite(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await api.delete("products", id);
+
+      setIsFavorite(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Container>
+      {isFavorite ? (
+        <IoHeart onClick={handleDelete} color="red" />
+      ) : (
+        <IoHeartOutline onClick={handleFavorite} color="red" />
+      )}
+    </Container>
+  );
+}
